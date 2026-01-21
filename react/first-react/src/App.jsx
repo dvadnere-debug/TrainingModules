c85c8255b959ba6b8909586c776d0b0ede6131d1
Divya <dvadnere@trueigtech.com>
2026-01-21 13:37:40 +0530

    starting react: covering the overview, jsx, virtual dom and components

---
+++ react/first-react/src/App.jsx
@@ -0,0 +1,20 @@
+// import { useState } from "react";
+
+// import "./App.css";
+
+// function App() {
+//   const [count, setCount] = useState(0);
+//   //const myElement = <h1> I Love JSX</h1>;
+
+//   return (
+//     <>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default App;
