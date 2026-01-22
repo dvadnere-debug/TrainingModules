7b6546a12935f622f46fbf79a69cec269caa95b5
Divya <dvadnere@trueigtech.com>
2026-01-22 15:25:18 +0530

    conditional rendering

--- react/first-react/src/components/UserGreeting.jsx
+++ react/first-react/src/components/UserGreeting.jsx
@@ -5,9 +5,15 @@ export default function UserGreeting(props) {
   //     <h2>Please log in to continue</h2>;
   //   }
 
-  return props.isLoggedIn ? (
-    <h2> Welcome {props.username}</h2>
-  ) : (
-    <h2>Please login to continue</h2>
-  );
+  //   return props.isLoggedIn ? (
+  //     <h2> Welcome {props.username}</h2>
+  //   ) : (
+  //     <h2>Please login to continue</h2>
+  //   );
+
+  const welcomeMessage = <h2>Welcome {props.username} </h2>;
+
+  const loginPrompt = <h2> Please login to continue </h2>;
+
+  return props.isLoggedIn ? welcomeMessage : loginPrompt;
 }
